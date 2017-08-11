2cc0bd8a32b0e50b2ff80a3001792b769e314ecd
jrdnndtsch <jordanbdeutsch@gmail.com>
2017-08-11 14:13:51 -0400

    config for aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,16 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  # routes, locales, etc. This feature depends on the listen gem.
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  Paperclip.options[:command_path] = "/usr/local/bin/"
+    config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_region => 'us-east-1',
+      :bucket => 'fbm2020-dev'
+    }
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
