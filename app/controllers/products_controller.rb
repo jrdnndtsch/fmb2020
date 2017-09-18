449d1e392071433235c8788e3b801f8e8dbe3640
jrdnndtsch <jordanbdeutsch@gmail.com>
2017-09-18 09:05:38 -0400

    test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,8 @@ class ProductsController < ShopifyApp::AuthenticatedController
 		
 		StoredProduct.all.each do |c|
 			if !c.posted
+				puts c.title
+				puts '>>>>>>>>>>>>>>>TITLE>>>>>>>>>>>>>'
 				tags = ''
 				metafields = []
 
