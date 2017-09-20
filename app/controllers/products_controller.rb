7942e2ea399730b1a4ce071b7044ea8a0e6d1429
jrdnndtsch <jordanbdeutsch@gmail.com>
2017-09-20 15:32:58 -0400

    remove publication ocation from metafileds to send

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -48,7 +48,7 @@ class ProductsController < ShopifyApp::AuthenticatedController
 
 				metafields << {"key" => "pages", "value" => c.page_number, "value_type" => "integer", "namespace" => "book_data" }
 				metafields << {"key" => "publication_date", "value" => c.publication_date.strftime("%m/%d/%Y"), "value_type" => "string", "namespace" => "book_data"} 
-				metafields << {"key" => "publication_location", "value" => c.publication_location, "value_type" => "string", "namespace" => "book_data"} 
+				# metafields << {"key" => "publication_location", "value" => c.publication_location, "value_type" => "string", "namespace" => "book_data"} 
 				metafields << {"key" => "rights_sold", "value" => c.rights_sold, "value_type" => "string", "namespace" => "book_data"} 
 				metafields << {"key" => "age_range_start", "value" => c.age_range_start, "value_type" => "integer", "namespace" => "book_data"} 
 				metafields << {"key" => "age_range_end", "value" => c.age_range_end, "value_type" => "integer", "namespace" => "book_data"}
