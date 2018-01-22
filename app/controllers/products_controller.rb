541b216e858bb0f48cdd7c93a6c8c26f3e90373c
jrdnndtsch <jordanbdeutsch@gmail.com>
2018-01-22 12:42:05 +0100

    prod id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -138,7 +138,7 @@ class ProductsController < ShopifyApp::AuthenticatedController
 
 			  case request.code
 				when 201
-					puts request.parsed_response.product.id
+					puts request.parsed_response["product"]["id"]
 					puts "RESUTL OF POST PROD<<<<<<<<<<<<<<<<<"
 			  		c.update(posted: true, shopify_product_id: request.parsed_response.product.id)
 			  		puts "#{c.title} was posted!"
