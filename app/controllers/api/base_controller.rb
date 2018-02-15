28748d279a4ea536e7d3c64439a779467732c8d5
jrdnndtsch <jordanbdeutsch@gmail.com>
2018-02-15 09:17:54 +0100

    access-control-request-moethod to resolve cors issue on chached request

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -9,6 +9,7 @@ class Api::BaseController < ActionController::Base
     	puts '>>>>>>>>>>>>>cors<<<<<<<<<<<<<<<<<'
       headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
       headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
       headers['Access-Control-Max-Age'] = "1728000"
     end
