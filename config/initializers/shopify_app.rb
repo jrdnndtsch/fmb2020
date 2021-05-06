eb1f6a0e30789a069461676c3faa082305f04255
trudy macnabb <trudy@unwrapit.com>
2021-05-06 15:55:13 -0400

    lowered shopifh app v

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -4,6 +4,5 @@ ShopifyApp.configure do |config|
   config.secret = ENV['api_secret']
   config.scope = "read_orders, read_products, write_products"
   config.embedded_app = true
-  config.session_repository = ::Shop
 end
 
