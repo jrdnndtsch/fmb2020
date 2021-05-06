cd66458010a56a9d707e11ccf5566747dfa191d4
trudy macnabb <trudy@unwrapit.com>
2021-05-06 15:43:37 -0400

    add namspace for ::Shop

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -4,5 +4,6 @@ ShopifyApp.configure do |config|
   config.secret = ENV['api_secret']
   config.scope = "read_orders, read_products, write_products"
   config.embedded_app = true
+  config.session_repository = ::Shop
 end
 
