32e4e0b0708439a39675a7ae74ddf377b58f3e56
trudy macnabb <trudy@unwrapit.com>
2021-05-11 15:04:50 -0400

    added back session repository init

--- config/initializers/shopify_session_repository.rb
+++ config/initializers/shopify_session_repository.rb
@@ -1,9 +1,9 @@
-# if Rails.configuration.cache_classes
-#   ShopifyApp::SessionRepository.storage = Shop
-# else
-#   reloader = defined?(ActiveSupport::Reloader) ? ActiveSupport::Reloader : ActionDispatch::Reloader
+if Rails.configuration.cache_classes
+  ShopifyApp::SessionRepository.storage = Shop
+else
+  reloader = defined?(ActiveSupport::Reloader) ? ActiveSupport::Reloader : ActionDispatch::Reloader
 
-#   reloader.to_prepare do
-#     ShopifyApp::SessionRepository.storage = Shop
-#   end
-# end
+  reloader.to_prepare do
+    ShopifyApp::SessionRepository.storage = Shop
+  end
+end
